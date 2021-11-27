332db9dddb9e4b5de282ee973e1b247c9492e609
shijie.chen <shijie.chen@shopee.com>
2021-11-28 00:39:35 +0800

    test1

---
+++ README.md
@@ -0,0 +1,7 @@
+
+## 学习资料
+python 教程
+
+(https://docs.python.org/zh-cn/3.6/tutorial/index.html)
+
+
---
+++ Test1.py
@@ -0,0 +1,43 @@
+"""
+题目1 计算
+计算算式结果：
+1、19整除3减去2
+2、4的平方加上8除以2
+
+题目2 字符串
+s = "Blue Green Black"
+1、使用s[]方法，分别输出Blue、Green、Black
+2、使用append或+号，在s中加新的颜色字符串: White，让s = "Blue Green Black White"
+3、使用pop或s[]方法，在s中删除颜色字符串：Green，让s = "Blue Black"
+
+题目3 列表
+list = [1, 1, 2, 3, 5, 8]
+1、在list最后追加一个数：13
+2、使用list[]，把数字2改成4
+3、从大到小排序list
+
+list = [ [1, 2, 3], [4, 5, 6], [7, 8, 9] ]
+1、按照3x3的形式输出列表：
+    [1, 2, 3]
+    [4, 5, 6]
+    [7, 8, 9]
+2、替换5为10
+
+题目4 函数
+定义一个函数，删除列表指定元素：
+ans = delete_list(nums, val)
+
+执行：
+nums = [3,2,2,3]
+val = 3
+print(delete_list(nums, val))
+
+输出：
+[2,2]
+
+提示：
+定义空列表 ans = []
+不等于 if a != b
+列表拼接 list1 + list2 或 list.append(i)
+"""
+
